5d5b344ab9eb6afacc88501fd4bbbe7da7c03d68
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2026-08-02 17:02:02 +0100

    feat: made use of the aos library.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import { useDripmatch } from "./components/Context"
 import Header from "./components/header/Header"
 import Homepage from "./pages/home/Homepage";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 
 function App() {
   const { isDark } = useDripmatch();
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,     // animation duration
+      //once: true,        // whether animation should happen only once
+      offset: 100,       // offset (px) from original trigger point
+    });
+  }, []);
 
   return (
     <div className={isDark ? '' : 'light-mode'}>
--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -5,25 +5,33 @@ import { FaLink } from 'react-icons/fa6'
 
 function Homepage() {
   return (
-    <div className="min-h-205 bg-radial-[at_50%_70%] from-black/70 via-(--bg-color3) to-(--bg-color) to-90% flex justify-center lg:px-20">
+    <div className="min-h-205 bg-radial-[at_50%_70%] from-black/70 via-(--bg-color3) to-(--bg-color) to-90% flex justify-center lg:px-20 overflow-hidden">
       <section className={`text-(--text-color) uppercase relative flex justify-between max-w-(--max-w) w-full py-10`}>
         <div className="flex flex-col justify-between lg:py-5">
-          <h1 className='font-bold'>Everyday <br/> Style</h1>
+          <h1 className='font-bold' data-aos="fade-right"
+            data-aos-anchor="#example-anchor"
+            data-aos-offset="500"
+            data-aos-duration="500">
+              Everyday <br/> Style
+          </h1>
           
           <div>
-            <p className='normal-case'>Discover elevated essentials and trend <br/> peices crafted to fit your lifestyle</p>
+            <p className='normal-case' >Discover elevated essentials and trend <br/> peices crafted to fit your lifestyle</p>
             <button className="bg-red-800 px-4 py-2 mt-7 text-white">Explore Collection</button>
           </div>
         </div>
 
         <div className='absolute right-[30%] bottom-0'>
-          <img src={heroPic1} alt="" className='lg:w-130'/>
+          <img src={heroPic1} alt="" className='lg:w-130' data-aos='zoom-in-up'/>
         </div>
 
         <div className='flex flex-col justify-end pb-5'>
-          <h1>Made <br/> seamleass</h1>
+          <h1 data-aos="fade-left"
+            data-aos-anchor="#example-anchor"
+            data-aos-offset="500"
+            data-aos-duration="500">Made <br/> seamleass</h1>
 
-          <div className='group w-50 p-2 bg-(--bg-color2) self-end'>
+          <div className='group w-50 p-2 bg-(--bg-color2) self-end' data-aos="zoom-in-left">
             <div className='relative'>
               <div className='absolute bg-black/80 w-full h-0 justify-center items-center translate-y-full opacity-0 flex group-hover:translate-y-0 group-hover:opacity-100 transition-all group-hover:h-full'>
                 <Link to={'/guide'} className='hover:underline'> 
