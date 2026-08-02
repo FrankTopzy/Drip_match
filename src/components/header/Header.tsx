a42dc588f0d2379f3d4edb8da17c60aad9f59f81
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-08-02 06:32:31 +0100

    build: designed the header.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,32 @@
+import { FaSun } from "react-icons/fa6"
+import { useDripmatch } from "../Context";
+
+
+function Header() {
+  const { isDark, setIsDark } = useDripmatch();
+
+  return (
+    <div className={`flex justify-center bg-linear-to-r from-(--bg-color) from-10% via-(--bg-color2) via-70% to-(--bg-color3) to-95%`}>
+      <header className="flex justify-between max-w-(--max-w) w-full py-5 text-(--text-color)">
+        <div className="uppercase italic">Dripmatch</div>
+
+        <div>
+          <ul className="flex gap-15">
+            <li>Home</li>
+            <li>Guide</li>
+            <li>Favorites</li>
+            <li>Settings</li>
+          </ul>
+        </div>
+
+        <div>
+          <button onClick={() => setIsDark(!isDark)}>
+            <FaSun/>
+          </button>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
