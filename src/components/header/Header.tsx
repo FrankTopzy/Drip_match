d295c6347b6c22f70a04ab51097ff78c6fe0acdd
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2026-08-02 20:39:20 +0100

    feat: implement homepage, laboratory, and header components for initial application layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import { FaSun } from "react-icons/fa6"
 import { useDripmatch } from "../Context";
+import Styles from './header.module.css'
 
 
 function Header() {
@@ -7,13 +8,13 @@ function Header() {
 
   return (
     <div className={`flex justify-center bg-linear-to-r from-(--bg-color) from-10% via-(--bg-color2) via-70% to-(--bg-color3) to-95% fixed w-full lg:px-20 z-99`} data-aos="fade-down">
-      <header className="flex justify-between max-w-(--max-w) w-full py-5 text-(--text-color)">
+      <header className="flex justify-between items-center max-w-(--max-w) w-full py-5 text-(--text-color)">
         <div className="uppercase italic">Dripmatch</div>
 
         <div>
-          <ul className="flex gap-15">
+          <ul className={`${Styles.nav_list} flex gap-15`}>
             <li>Home</li>
-            <li>Guide</li>
+            <li>Laboratory</li>
             <li>Favorites</li>
             <li>Settings</li>
           </ul>
