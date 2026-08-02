0d3055df25375d1cf039553941343e7b010df784
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-08-02 06:26:10 +0100

    displayed the header and homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import { useDripmatch } from "./components/Context"
+import Header from "./components/header/Header"
+import Homepage from "./pages/home/Homepage";
 
 
 function App() {
+  const { isDark } = useDripmatch();
 
   return (
-    <>
-     
-    </>
+    <div className={isDark ? '' : 'light-mode'}>
+     <Header/>
+     <Routes>
+      <Route path="/" element={<Homepage/>}/>
+     </Routes>
+    </div>
   )
 }
 
