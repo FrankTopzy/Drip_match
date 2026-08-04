df3b77f099237f7f8443b1c566ad7fb8efae6d9c
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-08-04 14:31:18 +0100

    Adjusted the styles.

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -5,9 +5,9 @@ import { FaArrowRight, FaLink } from 'react-icons/fa6'
 
 function Homepage() {
   return (
-    <div className="min-h-205 bg-radial-[at_50%_70%] from-black/70 via-(--bg-color3) to-(--bg-color) to-90% flex justify-center lg:px-20 overflow-hidden">
+    <div className="min-h-205 xl:min-h-250 bg-radial-[at_50%_70%] from-black/70 via-(--bg-color3) to-(--bg-color) to-90% flex justify-center lg:px-20 overflow-hidden">
       <section className={`text-(--text-color) uppercase relative flex justify-between max-w-(--max-w) w-full py-15`}>
-        <div className="flex flex-col justify-between lg:py-5">
+        <div className="flex flex-col justify-between lg:py-5 xl:py-15">
           <h1 className='font-bold' data-aos="fade-right"
             data-aos-anchor="#example-anchor"
             data-aos-offset="500"
@@ -17,14 +17,14 @@ function Homepage() {
           
           <div>
             <p className='normal-case' >Try Outfits On Instantly See how clothes look on you before <br/> you buy with our AI-powered virtual try-on experience.</p>
-            <button className="bg-red-800 px-4 py-2 mt-7 text-white uppercase flex items-center gap-2 group ">
+            <Link to="/lab" className="bg-red-800 px-4 py-2 mt-7 text-white uppercase flex items-center gap-2 group inline-flex">
               Go to Lab <FaArrowRight className='group-hover:translate-x-1 transition-all'/>
-            </button>
+            </Link>
           </div>
         </div>
 
-        <div className='absolute right-[30%] bottom-0 top-15'>
-          <img src={heroPic1} alt="" className='lg:w-130' data-aos='zoom-in-up'/>
+        <div className='absolute right-[30%] bottom-0'>
+          <img src={heroPic1} alt="" className='lg:w-130 xl:w-150' data-aos='zoom-in-up'/>
         </div>
 
         <div className='flex flex-col justify-end pb-5'>
