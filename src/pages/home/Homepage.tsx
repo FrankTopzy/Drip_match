a8f9aabc36f7c835813e1144b6855eea04e9fdf4
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-08-07 01:06:34 +0100

    feat: Adjusted the padding and margin.

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -7,8 +7,8 @@ function Homepage() {
   return (
     <div className="min-h-screen xl:min-h-250 bg-radial-[at_50%_70%] from-black/70 via-(--bg-color3) to-(--bg-color) to-90% flex justify-center lg:px-20 overflow-hidden">
       <section className={`text-(--text-color) uppercase relative flex justify-between max-w-(--max-w) w-full pt-15`}>
-        <div className="flex flex-col justify-between px-5 py-10 xl:py-15">
-          <p className='font-semibold text-3xl md:text-6xl text-center md:text-left md:w-50'
+        <div className="flex flex-col justify-between py-15 xl:py-15">
+          <p className='font-semibold text-4xl md:text-6xl px-5 md:px-0 text-center md:text-left md:w-50'
             data-aos="fade-right"
             data-aos-anchor="#example-anchor"
             data-aos-offset="500"
@@ -16,7 +16,7 @@ function Homepage() {
               Everyday Style <span className='md:hidden'>Made Seamless</span>
           </p>
 
-          <div className='md:hidden group w-50 p-2 bg-(--bg-color2) self-center m-5' data-aos="flip-left">
+          <div className='md:hidden group w-80 p-1 bg-(--bg-color2) self-center my-5' data-aos="flip-left">
             <div className='relative'>
               <div className='absolute bg-black/80 w-full h-0 justify-center items-center translate-y-full opacity-0 flex group-hover:translate-y-0 group-hover:opacity-100 transition-all group-hover:h-full'>
                 <Link to={'/guide'} className='hover:underline'> 
@@ -30,7 +30,7 @@ function Homepage() {
           </div>
           
           <div className='text-center md:text-left'>
-            <p className='normal-case md:w-110' >Try outfits on instantly and see how clothes look on you before you buy with our AI-powered virtual try-on experience.</p>
+            <p className='normal-case md:w-110 px-5 md:px-0' >Try outfits on instantly and see how clothes look on you before you buy with our AI-powered virtual try-on experience.</p>
             <Link to="/lab" className="bg-red-800 px-4 py-2 mt-5 text-white uppercase inline-flex items-center gap-2 group">
               Go to Lab <FaArrowRight className='group-hover:translate-x-1 transition-all'/>
             </Link>
