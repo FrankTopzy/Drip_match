3629ea9a93448dc6c189722954248571b3535bea
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-08-07 22:00:30 +0100

    feat: Saved the user's mode choice to localStorage.

--- src/components/Context.tsx
+++ src/components/Context.tsx
@@ -1,11 +1,26 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import type { DripmatchContextPropType } from "./types";
 
 
 const DripmatchContext = createContext<any>(null);
 
 export const DripmatchContextProvider = ({children}: DripmatchContextPropType) => {
-  const [isDark, setIsDark] = useState<boolean>(true)
+  const [isDark, setIsDark] = useState<boolean>(() => {
+    return localStorage.getItem('theme') === "darkMode"
+  });
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.remove('light-mode');
+      localStorage.setItem('theme', 'darkMode')
+    } else {
+       document.documentElement.classList.add('light-mode');
+       localStorage.setItem('theme', 'lightMode')
+    }
+
+    //console.log(localStorage.getItem('theme'));
+    
+  }, [isDark])
 
   const value = {
     isDark,
