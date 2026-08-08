e8be283c80ea6063281272c24a7a161bcec2295b
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-08-08 02:17:05 +0100

    feat: Changed the appearance of the navbar/header.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,33 +1,38 @@
-import { FaSun } from "react-icons/fa6"
+import { FaMoon, FaSun } from "react-icons/fa6"
 import { useDripmatch } from "../Context";
 import Styles from './header.module.css'
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
+type IsActiveType = 'home' | 'lab' | 'favorites' | 'drafts' | 'settings';
 
 function Header() {
   const { isDark, setIsDark } = useDripmatch();
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [isActive, setIsActive] = useState<IsActiveType>('home')
   
 
   return (
     <div className={`flex justify-center bg-linear-to-r from-(--bg-color) from-10% via-(--bg-color2) via-70% to-(--bg-color3) to-95% fixed w-full lg:px-20 z-10`} data-aos="fade-down">
       <header className="flex justify-between items-center max-w-(--max-w) w-full px-5 md:px-0 py-5 text-(--text-color) relative">
-        <Link to={'/'} className={`${Styles.logo} uppercase text-2xl md:text-3xl`}>Dripmatch</Link>
+        <Link to={'/'} className={`${Styles.logo} uppercase text-2xl md:text-3xl text-shadow-lg/30 hover:scale-105 transition-all`}><span className="text-4xl font-sans italic md:text-5xl">D</span>ripmatch</Link>
 
-        <div className="hidden md:block">
-          <ul className={`${Styles.nav_list} flex gap-15`}>
-            <li><Link to={'/'}>Home</Link></li>
-            <li><Link to={'/lab'}>Laboratory</Link></li>
-            <li><Link to={'/favorites'}>Favorites</Link></li>
-            <li><Link to={'/drafts'}>[Drafts]</Link></li>
-            <li><Link to={'/settings'}>Settings</Link></li>
+        <div className="hidden md:block border border-slate-700 rounded-full px-6 py-7">
+          <ul className={`${Styles.nav_list} flex gap-5`}>
+            <li><Link to={'/'} onClick={() => setIsActive('home')} className={`${isActive === 'home' && `${Styles.isActive}`}`}>Home</Link></li>
+
+            <li><Link to={'/lab'} onClick={() => setIsActive('lab')} className={`${isActive === 'lab' && `${Styles.isActive}`}`}>Laboratory</Link></li>
+
+            <li><Link to={'/favorites'} onClick={() => setIsActive('favorites')} className={`${isActive === 'favorites' && `${Styles.isActive}`}`}>Favorites</Link></li>
+
+            <li><Link to={'/drafts'} onClick={() => setIsActive('drafts')} className={`${isActive === 'drafts' && `${Styles.isActive}`}`}>[Drafts]</Link></li>
+            <li><Link to={'/settings'} onClick={() => setIsActive('settings')} className={`${isActive === 'settings' && `${Styles.isActive}`}`}>Settings</Link></li>
           </ul>
         </div>
 
         <div className="flex gap-3">
-          <button onClick={() => setIsDark(!isDark)}>
-            <FaSun/>
+          <button onClick={() => setIsDark(!isDark)} className="text-md">
+           {isDark ? (<FaSun/>) : (<FaMoon className=""/>)}
           </button>
 
           <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden flex flex-col gap-1.5 cursor-pointer bg-transparent border-0 p-1">
