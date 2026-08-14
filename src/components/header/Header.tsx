402520705e080a4046b934e236884ceab9c43695
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-08-14 04:11:03 +0100

    feat: reduced the padding and redesigned the mobile nav menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -29,20 +29,20 @@ function Header() {
 
 
   return (
-    <div className={`flex justify-center pt-5 to-95% fixed w-full px-4 lg:px-20 z-99`} data-aos="fade-down">
-      <header className="flex justify-between items-center max-w-(--max-w) w-full px-5 lg:px-9 py-2 text-(--text-color) relative bg-(--navbg-color) rounded-full backdrop-blur-xl border border-(--text-color)/20">
+    <div className={`flex justify-center pt-5 to-95% fixed w-full lg:px-20 z-99`} data-aos="fade-down">
+      <header className="flex justify-between items-center max-w-(--max-w) mx-4 md:mx-0 w-full px-5 lg:px-9 py-2 text-(--text-color) relative bg-(--navbg-color) rounded-full backdrop-blur-xl border border-(--text-color)/20">
         <Link to={'/'} className={`${Styles.logo} uppercase text-2xl xl:text-3xl text-shadow-lg/30 hover:scale-105 transition-all`}><span className="text-4xl lg:text-5xl font-sans italic">D</span>ripmatch</Link>
 
         <div className="hidden md:block border border-slate-700 rounded-full px-3 py-3">
           <ul className={`${Styles.nav_list} flex gap-3 xl:gap-5`}>
-            <li><Link to={'/'} onClick={() => setIsActive('')} className={`${isActive === '' && `${Styles.isActive}`}`}>Home</Link></li>
+            <li><Link to={'/'} className={`${isActive === '' && `${Styles.isActive}`}`}>Home</Link></li>
 
-            <li><Link to={'/lab'} onClick={() => setIsActive('lab')} className={`${isActive === 'lab' && `${Styles.isActive}`}`}>Laboratory</Link></li>
+            <li><Link to={'/lab'} className={`${isActive === 'lab' && `${Styles.isActive}`}`}>Laboratory</Link></li>
 
-            <li><Link to={'/favorites'} onClick={() => setIsActive('favorites')} className={`${isActive === 'favorites' && `${Styles.isActive}`}`}>Favorites</Link></li>
+            <li><Link to={'/favorites'} className={`${isActive === 'favorites' && `${Styles.isActive}`}`}>Favorites</Link></li>
 
-            <li><Link to={'/drafts'} onClick={() => setIsActive('drafts')} className={`${isActive === 'drafts' && `${Styles.isActive}`}`}>[Drafts]</Link></li>
-            <li><Link to={'/settings'} onClick={() => setIsActive('settings')} className={`${isActive === 'settings' && `${Styles.isActive}`}`}>Settings</Link></li>
+            <li><Link to={'/drafts'} className={`${isActive === 'drafts' && `${Styles.isActive}`}`}>[Drafts]</Link></li>
+            <li><Link to={'/settings'} className={`${isActive === 'settings' && `${Styles.isActive}`}`}>Settings</Link></li>
           </ul>
         </div>
 
@@ -57,21 +57,17 @@ function Header() {
             <span className={`block w-6 h-0.5 bg-zinc-800 transition-transform ${menuOpen ? '-rotate-45 -translate-y-2' : ''}`}></span>
           </button>
         </div>
-
-        {
-          menuOpen && (
-            <div className="absolute md:hidden bg-(--bg-color) h-screen left-0 top-full w-full z-50 mt-5">
-              <ul className={`p-5 flex flex-col gap-15`}>
-                <Link to={'/'} onClick={() => setMenuOpen(false)}>Home</Link>
-                <Link to={'/lab'} onClick={() => setMenuOpen(false)}>Laboratory</Link>
-                <Link to={'/favorites'} onClick={() => setMenuOpen(false)}>Favorites</Link>
-                <Link to={'/drafts'} onClick={() => setMenuOpen(false)}>[Drafts]</Link>
-                <Link to={'/settings'} onClick={() => setMenuOpen(false)}>Settings</Link>
-              </ul>
-            </div>
-          )
-        }
       </header>
+
+      <div className={`${!menuOpen ? 'right-full' : 'right-0 w-full md:hidden bg-(--bg-color) mt-5 z-50 text-(--text-color)'} absolute top-full h-screen  transition-all`}>
+        <ul className={`p-5 flex flex-col gap-15`}>
+          <Link to={'/'} onClick={() => setMenuOpen(false)} className="hover:bg-(--nav-list-bg) hover:text-(--bg-color) px-4 py-2 rounded-full transition-all">Home</Link>
+          <Link to={'/lab'} onClick={() => setMenuOpen(false)} className="hover:bg-(--nav-list-bg) hover:text-(--bg-color) px-4 py-2 rounded-full hover:transition-all">Laboratory</Link>
+          <Link to={'/favorites'} onClick={() => setMenuOpen(false)} className="hover:bg-(--nav-list-bg) hover:text-(--bg-color) px-4 py-2 rounded-full hover:transition-all">Favorites</Link>
+          <Link to={'/drafts'} onClick={() => setMenuOpen(false)} className="hover:bg-(--nav-list-bg) hover:text-(--bg-color) px-4 py-2 rounded-full hover:transition-all">[Drafts]</Link>
+          <Link to={'/settings'} onClick={() => setMenuOpen(false)} className="hover:bg-(--nav-list-bg) hover:text-(--bg-color) px-4 py-2 rounded-full hover:transition-all">Settings</Link>
+        </ul>
+      </div>
     </div>
   )
 }
